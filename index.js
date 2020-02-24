a37983cbf88c6b351682449ac24e7615f2ba810f
hank.liu <hank.liu@program.com.tw>
2020-02-24 17:38:23 +0800

    deplay to GCS version

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ let getCsv = async url => {
 }
 
 const CSV_MASK = 'https://data.nhi.gov.tw/resource/mask/maskdata.csv'
-let getMasks = async () => {
+exports.getMasks = async () => {
   const masks = await getCsv(CSV_MASK)
   console.log(`取得 ${masks.length} 筆口罩數量資料`)
   const mask = _.fromPairs(_.map(masks, mask => [
@@ -36,7 +36,7 @@ let getMasks = async () => {
 }
 
 const CSV_STORE = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT4tWc7zUcHQQ8LC_0276aOZNcIBu544YB9XrSRz7oq66q5lE3RHN5Ix2-4S3NL4bL-0zi5nKzE13eX/pub?gid=0&single=true&output=csv'
-const getStores = async () => {
+exports.getStores = async () => {
   const stores = await getCsv(CSV_STORE)
   console.log(`取得 ${stores.length} 筆店家資料`)
   return stores
@@ -48,18 +48,17 @@ const unparseCsv = (data) => {
   })
 }
 
-const uploadCSV = async (mask, maxAge=30)=>{
+exports.uploadCSV = async (mask, maxAge=30)=>{
   const {Storage} = require('@google-cloud/storage');
   const projectId = getenv('projectId')
-  const keyFile = getenv('keyFile')
+  const keyFilename = getenv('keyFilename')
   const maskBucket = getenv('maskBucket')
   const maskFile = getenv('maskFile')
 
-  const storage = new Storage({projectId, keyFile});
-  const [buckets] = await storage.getBuckets();
+  const storage = new Storage({projectId, keyFilename});
   const file = storage.bucket(maskBucket).file(maskFile);
-  // console.log(mask)
-  await file.save( mask ,{
+  console.log(mask)
+  return await file.save( mask ,{
     gzip: true,
     validation:'crc32c',
     matadata:{
@@ -68,33 +67,26 @@ const uploadCSV = async (mask, maxAge=30)=>{
       contentLanguage: 'zh',
     },
   })
-  
 }
 
-setTimeout(function () {}, 1000)
 
-const sleep = t => new Promise(resolve => { setTimeout(resolve, t) })
-
-const main = async () =>{
+exports.scheduler = async () =>{
   const [masks, stores] = await Promise.all([
-    getMasks(), getStores()
+    exports.getMasks(), exports.getStores()
   ])
   // console.log(stores)
-  _.each(stores,row=>{
+  _.each(stores,row=>{    
     const mask = _.get(masks, row.id)
     if(!_.isNil(mask)){
       row['adult']=_.parseInt(masks[row.id]['adult'])
       row['child']=_.parseInt(masks[row.id]['child'])
-      row['mask_updated']=masks[row.id]['mask_updated']
+      row['mask_updated']=new Date(masks[row.id]['mask_updated']).getTime()
     }
   })
-
-  return await uploadCSV(unparseCsv(stores))
+  return await exports.uploadCSV(unparseCsv(stores))
 }
 
-Promise.all([
-  main()
-])
 
-
+Promise.all([exports.scheduler()])
+.catch(onRejected=>console.log(onRejected));
 
